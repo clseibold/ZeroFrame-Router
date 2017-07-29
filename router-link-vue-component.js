3e5999233122a8bc77d392ca21fec3f0d8c0d47a
Christian Seibold <krixano@outlook.com>
2017-07-28 19:45:30 -0500

    Small change

--- router-link-vue-component.js
+++ router-link-vue-component.js
@@ -1,18 +1,9 @@
 Vue.component('route-link', {
 	props: ['to'],
-	template: '<a v-on:click="goto" v-bind:class="{ \'is-active\': active }"><slot></slot></a>',
+	template: '<a v-on:click="goto"><slot></slot></a>',
 	methods: {
 		goto: function() {
-			//console.log(typeof this.to);
 			Router.navigate(this.to);
-		}
-	},
-	computed: {
-		active: function() {
-			if (Router.currentRoute === this.to) {
-				return true;
-			}
-			return false;
 		}
 	}
 });
